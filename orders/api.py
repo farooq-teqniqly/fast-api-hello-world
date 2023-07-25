15a7ea257edd7c8ce83437ad01edab4ddf9d8369
farooq <farooq@teqniqly.com>
2023-07-25 13:38:53 -0700

    Orders API.

---
+++ orders/api.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from uuid import UUID
+from starlette.responses import Response
+from starlette import status
+from orders.app import app
+
+orders = {
+    "3d82eea9-7498-430f-9344-7fca4580fa62": {
+        "id": "3d82eea9-7498-430f-9344-7fca4580fa62",
+        "status": "delivered",
+        "created": datetime.utcnow(),
+        "items": {
+            "product": "cappuccino",
+            "size": "medium",
+            "quantity": 1
+        }
+    },
+    "553ed79e-0542-46e6-9761-41fc1eed6005": {
+        "id": "3d82eea9-7498-430f-9344-7fca4580fa62",
+        "status": "delivered",
+        "created": datetime.utcnow(),
+        "items": {
+            "product": "cappuccino",
+            "size": "medium",
+            "quantity": 1
+        }
+    }
+}
+
+
+@app.get("/orders", status_code=status.HTTP_200_OK)
+def get_orders():
+    return {"orders": orders}
+
+
+@app.post("/orders", status_code=status.HTTP_201_CREATED)
+def create_order():
+    return orders["3d82eea9-7498-430f-9344-7fca4580fa62"]
+
+
+@app.get("/orders/[order_id]")
+def get_order(order_id: UUID):
+    order = orders.get(str(order_id))
+
+    if order is None:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+    return order
